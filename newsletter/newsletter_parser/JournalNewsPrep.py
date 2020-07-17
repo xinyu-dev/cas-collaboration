19ea0c1eb7a1cbebd11d67c8fc0d500b960d4c6d
Xin Yu <60579067+xinyu-dev@users.noreply.github.com>
2020-07-17 19:46:12 -0400

    added files

---
+++ newsletter/newsletter_parser/JournalNewsPrep.py
@@ -0,0 +1,12 @@
+"""
+journal_news_constants.py
+Author: X Yu, Meng Wu
+Date: 7-17-20
+
+Description: Static assets for generating the template for "journal news announcement" emails. Most of these elements do not neeed to be modified often.
+
+Note:
+    1. ALL static assets (css. js, img ...) need to be hosted on CDN (Cloudinary). You only need to pass their url links to the html template
+"""
+
+# 
